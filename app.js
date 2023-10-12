1c491a7eac6b230b04bc179181b34986dacac4bf
Shivang Yadav <shivangyadav121@gmail.com>
2023-10-12 20:33:19 +0530

    UPDATED

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   }),
